e384872be7419be647dbae38bda14629590ddf66
Killingova <ms.zhivkova@googlemail.com>
2025-04-04 14:06:57 +0200

    GitHub Pages Deployment Workflow hinzugefügt

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CYRLGAME/', // ✅ GANZ WICHTIG für GitHub Pages
   plugins: [react()],
 })
